b43c91397f3e6f6ee959a3e401cee9f491aa9603
NicolaM94 <nicola.moro2312@gmail.com>
2020-05-31 02:54:20 +0200

    Rebooting and changin

---
+++ dist/contacts_window.py
@@ -0,0 +1,5 @@
+import tkinter as tk
+
+class ContactsWindow :
+    def __init__(self,level_one):
+        self.level_one = level_one
---
+++ dist/message_window.py
@@ -0,0 +1,6 @@
+import tkinter as tk
+import json
+
+class MessageWindow:
+    def __init__(self,level_one):
+        self.level_one = level_one
