9b7edeb72c6866b6a182830f0874a041cec2bd8c
NicolaM94 <nicola.moro2312@gmail.com>
2020-06-01 01:28:43 +0200

    Pushing changes

--- MailBot.py
+++ MailBot.py
@@ -18,29 +18,29 @@ class MailBot:
 
         def message_button_func ():
             subengine = tk.Toplevel()
-            level_one = MessageWindow(subengine)
+            MessageWindow(subengine)
             subengine.mainloop()
         
         def contacts_button_func ():
             subengine = tk.Toplevel()
-            level_one = ContactsWindow(subengine)
+            ContactsWindow(subengine)
             subengine.mainloop()
         
         def settings_button_func ():
             subengine = tk.Toplevel()
-            level_one = SettingsWindow(subengine)
+            SettingsWindow(subengine)
             subengine.mainloop()
         
         def assistance_button_func():
-            webbrowser.open("https://github.com/NicolaM94/Automail/blob/master/README.md")
+            webbrowser.open("https://github.com/NicolaM94/MailBot/blob/master/README.md")
         
-        space_label = tk.Label().pack()
+        tk.Label().pack()
         self.message_button = tk.Button(text="Nuovo messaggio",height=2,width=15,command=message_button_func).pack(pady=5)
         self.contacts_button = tk.Button(text="Contatti",height=2,width=15,command=contacts_button_func).pack(pady=5)
         self.settings_button = tk.Button(text="Impostazioni",height=2,width=15,command=settings_button_func).pack(pady=5)
         self.assistance_button = tk.Button(text="Assistenza",height=2,width=15,command=assistance_button_func).pack(pady=5)
         self.exit_button = tk.Button(text="Esci",height=2,width=15,command=engine.destroy).pack(pady=5)
-        end_space_label = tk.Label().pack()
+        tk.Label().pack()
 
 
 
